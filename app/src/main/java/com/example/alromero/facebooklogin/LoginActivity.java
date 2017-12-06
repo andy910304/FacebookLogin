4d9e674915ebc5e63f88fa6d8322b0cfb4ba2627
Andy Luis Hernandez Romero <andy910304@gmail.com>
2017-12-06 13:22:02 -0500

    app semi-finished

--- app/src/main/java/com/example/alromero/facebooklogin/LoginActivity.java
+++ app/src/main/java/com/example/alromero/facebooklogin/LoginActivity.java
@@ -3,13 +3,19 @@ package com.example.alromero.facebooklogin;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.lang.reflect.Array;
 import java.util.Arrays;
@@ -18,6 +24,8 @@ public class LoginActivity extends AppCompatActivity {
 
     private LoginButton loginButton;
     private CallbackManager callbackManager;
+
+    String email, gender, name ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,8 +41,28 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onSuccess(LoginResult loginResult) {
 
-                goMainScreen();
-                
+                GraphRequest request = GraphRequest.newMeRequest(
+
+                        loginResult.getAccessToken(),
+                        new GraphRequest.GraphJSONObjectCallback(){
+
+                            @Override
+                            public void onCompleted(JSONObject object, GraphResponse response) {
+                                Log.v("Main", response.toString());
+                                setProfileToView(object);
+                                goMainScreen();
+                            }
+                        }
+
+                );
+
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,name,email,gender, birthday");
+                request.setParameters(parameters);
+                request.executeAsync();
+
+
+
             }
 
             @Override
@@ -57,6 +85,9 @@ public class LoginActivity extends AppCompatActivity {
 
         Intent intent = new Intent(this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        Bundle bolsa = new Bundle();
+        bolsa.putString("email", email);
+        intent.putExtras(bolsa);
         startActivity(intent);
 
     }
@@ -65,5 +96,19 @@ public class LoginActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public void setProfileToView(JSONObject profileToView) {
+        try {
+
+            email = profileToView.getString("email");
+            gender = profileToView.getString("gender");
+            name = profileToView.getString("name");
+
+        } catch (JSONException e){
+
+            e.printStackTrace();
+
+        }
     }
 }
--- app/src/main/java/com/example/alromero/facebooklogin/MainActivity.java
+++ app/src/main/java/com/example/alromero/facebooklogin/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.facebook.AccessToken;
 import com.facebook.FacebookSdk;
@@ -14,10 +15,17 @@ import com.facebook.login.LoginManager;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView email;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        email = (TextView) findViewById(R.id.textView);
+        Bundle bolsaR = getIntent().getExtras();
+        email.setText(bolsaR.getString("email"));
+
 
         if(AccessToken.getCurrentAccessToken()==null){
 
